ec9641dc3a477ade8499ba9773c5d5507900a0a4
Zhu Jieyong <j_zsh@MacBook-Pro.local>
2023-03-10 10:58:18 +0900

    Add more API

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 from typing import List
 
-=======
->>>>>>> 578de4c (Add API: menu CRUD)
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from app.db import session, Base
@@ -28,7 +25,6 @@ class DrinkModel(BaseModel):
     price: float
 
 
-<<<<<<< HEAD
 class OrderItemModel(BaseModel):
     item_type: int
     item_id: int
@@ -42,8 +38,6 @@ class OrderModel(BaseModel):
     order_items: List[OrderItemModel]
 
 
-=======
->>>>>>> 578de4c (Add API: menu CRUD)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -158,6 +152,17 @@ def get_menus():
 def create_customer(seat_id: int):
     try:
         seat = session.query(Seat).filter(Seat.seat_id == seat_id).first()
+        if not seat:
+            return JSONResponse(content={
+                'result': {},
+                'errors': [
+                    {
+                        'error_code': 400,
+                        'error_message': 'This seat does not exist.',
+                    }
+                ]
+            })
+        
         if not seat.is_available:
             return JSONResponse(content={
                 'result': {},
@@ -175,7 +180,6 @@ def create_customer(seat_id: int):
         # insert customer_id and seat_id into customers table
         new_customer = Customer(customer_id=customer_id, seat_id=seat_id)
         session.add(new_customer)
-        session.commit()
 
         # update seat_id to unavailable
         seat = session.query(Seat).filter(Seat.seat_id == seat_id).first()
@@ -187,6 +191,192 @@ def create_customer(seat_id: int):
                 'customer_id': str(customer_id),
             },
             'errors': [],
+        }
+        return JSONResponse(content=response)
+
+    except Exception as e:
+        session.rollback()
+        response = {
+            'result': {},
+            'errors': [
+                {
+                    'error_code': 500,
+                    'error_message': str(e),
+                }
+            ]
+        }
+        return JSONResponse(content=response)
+
+
+@app.get("/customers/{customer_id}/orders")
+def get_customer_orders(customer_id: str):
+    try:
+        _orders = session.query(Order).filter(
+            Order.customer_id == customer_id).all()
+        orders = []
+        
+        for order in _orders:
+            _order_detail = session.query(OrderDetail).filter(
+                OrderDetail.order_id == order.order_id).all()
+            order_details = []
+
+            for order_detail in _order_detail:
+                if order_detail.item_type == 0:
+                    order_details.append({
+                        'order_detail_id': order_detail.order_detail_id,
+                        'item_type': order_detail.item_type,
+                        'item_id': order_detail.item_id,
+                        'topping': order_detail.topping,
+                        'size': order_detail.size,
+                        'quantity': order_detail.quantity,
+                        'has_wasabi': order_detail.has_wasabi,
+                        'price': str(order_detail.price),
+                        'status': order_detail.status,
+                        'ordered_at': str(order_detail.ordered_at),
+                    })
+                else:
+                    order_details.append({
+                        'order_detail_id': order_detail.order_detail_id,
+                        'item_type': order_detail.item_type,
+                        'item_id': order_detail.item_id,
+                        'quantity': order_detail.quantity,
+                        'price': str(order_detail.price),
+                        'status': order_detail.status,
+                        'ordered_at': str(order_detail.ordered_at),
+                    })
+            
+            # compute total price
+            total_price = 0
+            for order_detail in order_details:
+                total_price += float(order_detail['price']) * order_detail['quantity']
+            
+
+            orders.append({
+                'order_id': order.order_id,
+                'customer_id': order.customer_id,
+                'status': order.status,
+                'total_price': total_price,
+                'order_items': order_details
+            })
+
+        response = {
+            'result': {
+                'orders': orders,
+            },
+            'errors': [],
+        }
+        return JSONResponse(content=response)
+    
+    except Exception as e:
+        response = {
+            'result': {},
+            'errors': [
+                {
+                    'error_code': 500,
+                    'error_message': str(e),
+                }
+            ]
+        }
+        return JSONResponse(content=response)
+
+
+@app.post("/customers/{customer_id}/orders")
+def create_customer_order(customer_id: str, order: OrderModel):
+    try:
+        # insert a new order to orders table and get the order_id
+        new_order = Order(customer_id=customer_id, status=0, total_price=0)
+        session.add(new_order)
+        session.flush()
+        order_id = new_order.order_id
+        # create order_details
+        for order_item in order.order_items:
+            # only sushi has topping, size and has_wasabi
+            if order_item.item_type == 0:
+                sushi = session.query(Sushi).filter(
+                    Sushi.sushi_id == order_item.item_id).first()
+                price = sushi.price * order_item.quantity
+                _order_detail = OrderDetail(
+                    order_id=order_id,
+                    item_type=order_item.item_type,
+                    item_id=order_item.item_id,
+                    topping=order_item.topping,
+                    size=order_item.size,
+                    quantity=order_item.quantity,
+                    has_wasabi=order_item.has_wasabi,
+                    price=price,
+                    status=1
+                )
+            else:
+                drink = session.query(Drink).filter(
+                    Drink.drink_id == order_item.item_id).first()
+                price = drink.price * order_item.quantity
+                _order_detail = OrderDetail(
+                    order_id=order_id,
+                    item_type=order_item.item_type,
+                    item_id=order_item.item_id,
+                    quantity=order_item.quantity,
+                    price=price,
+                    status=1
+                )
+            session.add(_order_detail)
+        session.commit()
+
+    except Exception as e:
+        session.rollback()
+        response = {
+            'result': {},
+            'errors': [
+                {
+                    'error_code': 500,
+                    'error_message': str(e),
+                }
+            ]
+        }
+        return JSONResponse(content=response)
+
+
+@app.get("/admin/orders/new")
+def get_new_orders():
+    # new order: order_details.status == 1
+    try:
+        # get all new orders, order_details.status == 1
+        # order_details join orders join customers join seats
+        order_details = session.query(OrderDetail).filter(
+            OrderDetail.status == 1).all()
+
+        orders = []
+        for order_detail in order_details:
+            # find which order this order_detail belongs to
+            order = session.query(Order).filter(
+                Order.order_id == order_detail.order_id).first()
+            # find which customer this order belongs to
+            customer = session.query(Customer).filter(
+                Customer.customer_id == order.customer_id).first()
+            # find which seat this order belongs to
+            seat = session.query(Seat).filter(
+                Seat.seat_id == customer.seat_id).first()
+
+            order = {
+                'order_id': order.order_id,
+                'seat_id': seat.seat_id,
+                'order_detail_id': order_detail.order_detail_id,
+                'customer_id': customer.customer_id,
+                'item_type': order_detail.item_type,
+                'item_id': order_detail.item_id,
+                'topping': order_detail.topping,
+                'size': order_detail.size,
+                'quantity': order_detail.quantity,
+                'has_wasabi': order_detail.has_wasabi,
+                'price': str(order_detail.price),
+                'status': order_detail.status,
+                'ordered_at': str(order_detail.ordered_at),
+            }
+            orders.append(order)
+        response = {
+            'result': {
+                'orders': orders
+            },
+            'errors': []
         }
         return JSONResponse(content=response)
 
@@ -203,29 +393,98 @@ def create_customer(seat_id: int):
         return JSONResponse(content=response)
 
 
-@app.get("/customers/{customer_id}/orders")
-def get_customer_orders(customer_id: str):
-    pass
-
-
-@app.post("/customers/{customer_id}/orders")
-def create_customer_order(customer_id: str, order: dict):
-    pass
-
-
-@app.get("/admin/orders/new")
-def get_new_orders():
-    pass
-
 
 @app.get("/admin/orders")
 def get_orders_by_customer_id(customer_id: str):
-    pass
+    try:
+        _orders = session.query(Order).filter(
+            Order.customer_id == customer_id).all()
+        orders = []
+        
+        for order in _orders:
+            _order_detail = session.query(OrderDetail).filter(
+                OrderDetail.order_id == order.order_id).all()
+            order_details = []
+
+            for order_detail in _order_detail:
+                if order_detail.item_type == 0:
+                    order_details.append({
+                        'order_detail_id': order_detail.order_detail_id,
+                        'item_type': order_detail.item_type,
+                        'item_id': order_detail.item_id,
+                        'topping': order_detail.topping,
+                        'size': order_detail.size,
+                        'quantity': order_detail.quantity,
+                        'has_wasabi': order_detail.has_wasabi,
+                        'price': str(order_detail.price),
+                        'status': order_detail.status,
+                        'ordered_at': str(order_detail.ordered_at),
+                    })
+                else:
+                    order_details.append({
+                        'order_detail_id': order_detail.order_detail_id,
+                        'item_type': order_detail.item_type,
+                        'item_id': order_detail.item_id,
+                        'quantity': order_detail.quantity,
+                        'price': str(order_detail.price),
+                        'status': order_detail.status,
+                        'ordered_at': str(order_detail.ordered_at),
+                    })
+            
+            # compute total price
+            total_price = 0
+            for order_detail in order_details:
+                total_price += float(order_detail['price']) * order_detail['quantity']
+            
+
+            orders.append({
+                'order_id': order.order_id,
+                'customer_id': order.customer_id,
+                'status': order.status,
+                'total_price': total_price,
+                'order_items': order_details
+            })
+
+        response = {
+            'result': {
+                'orders': orders,
+            },
+            'errors': [],
+        }
+        return JSONResponse(content=response)
+    
+    except Exception as e:
+        response = {
+            'result': {},
+            'errors': [
+                {
+                    'error_code': 500,
+                    'error_message': str(e),
+                }
+            ]
+        }
+        return JSONResponse(content=response)
 
 
 @app.put("/admin/order_details/{order_detail_id}")
 def update_order_detail(order_detail_id: int, status: int):
-    pass
+    try:
+        order_detail = session.query(OrderDetail).filter(
+            OrderDetail.order_detail_id == order_detail_id).first()
+        order_detail.status = status
+        session.commit()
+        return None
+    except Exception as e:
+        session.rollback()
+        return JSONResponse(content={
+            'result': {},
+            'errors': [
+                {
+                    'error_code': 500,
+                    'error_message': str(e),
+                }
+            ]
+        })
 
 
 @app.post("/admin/menus/sushi")
